48e4a6a13858584a5c70b1a234353a5b8ee55bdf
Stephen Ostapenko <stepanos2002@gmail.com>
2021-12-16 22:05:20 +0300

    version for testing

--- lab5_util.h
+++ lab5_util.h
@@ -3,8 +3,47 @@
 #include <omp.h>
 #include <stdexcept>
 
-#define SCHEDULE_TYPE static
+//#define SCHEDULE_TYPE static
 //#define SCHEDULE_CHUNK_SIZE
+
+#ifdef ST
+#define SCHEDULE_TYPE static
+#endif
+
+#ifdef DYN
+#define SCHEDULE_TYPE dynamic
+#endif
+
+#ifdef CH_0
+#endif
+
+#ifdef CH_1
+#define SCHEDULE_CHUNK_SIZE 1
+#endif
+
+#ifdef CH_2
+#define SCHEDULE_CHUNK_SIZE 2
+#endif
+
+#ifdef CH_4
+#define SCHEDULE_CHUNK_SIZE 4
+#endif
+
+#ifdef CH_8
+#define SCHEDULE_CHUNK_SIZE 8
+#endif
+
+#ifdef CH_10
+#define SCHEDULE_CHUNK_SIZE 10
+#endif
+
+#ifdef CH_16
+#define SCHEDULE_CHUNK_SIZE 16
+#endif
+
+#ifdef CH_32
+#define SCHEDULE_CHUNK_SIZE 32
+#endif
 
 #ifdef SCHEDULE_CHUNK_SIZE
 
